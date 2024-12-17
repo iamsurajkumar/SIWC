e2f0cb89bda731b977233dc2a04ada07b737047f
Suraj Kumar <surajdhunna@gmail.com>
2024-12-17 18:21:12 -0400

    completed the day2

---
+++ my_functions.py
@@ -0,0 +1,9 @@
+def say_hello(name):
+    return f"Hello, {name}!"
+
+def main():
+    print("Running directly!")
+    print(say_hello("World"))
+
+if __name__ == "__main__":
+    main()
---
+++ use_functions.py
@@ -0,0 +1,4 @@
+from my_functions import say_hello
+
+print("I'm using the function from my_functions.py:")
+print(say_hello("Friend"))
